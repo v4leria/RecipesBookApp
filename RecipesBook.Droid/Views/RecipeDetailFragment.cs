819dce62d69f86954a2979027806530a87abc2dd
Valeria Plutino <valeria.plutino@uts.edu.au>
2018-09-05 14:36:16 +1000

    Fixed Fragment navigation

--- RecipesBook.Droid/Views/RecipeDetailFragment.cs
+++ RecipesBook.Droid/Views/RecipeDetailFragment.cs
@@ -11,12 +11,14 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using MvvmCross.Binding.BindingContext;
 using MvvmCross.Droid.Support.V4;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
 using RecipesBook.Core.ViewModel;
-
 namespace RecipesBook.Droid.Views
 {
+    [MvxFragmentPresentation(typeof(HomeViewModel),Resource.Id.detailview, AddToBackStack = true)]
     public class RecipeDetailFragment : MvxFragment<RecipeDetailViewModel>
     {
         public override void OnCreate(Bundle savedInstanceState)
@@ -31,7 +33,14 @@ namespace RecipesBook.Droid.Views
 
             var view = this.BindingInflate(Resource.Layout.RecipeDetailLayout, container, false);
 
+            Activity.Title = ViewModel.Name;
+
             return view;
+        }
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            Activity.Title = "Recipes";
         }
     }
 }
